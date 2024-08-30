543960147db0282f9d07224ebcb66433684951f6
Cristian-Eduardo-Salazar1096 <164533888+Cristian-Eduardo-Salazar1096@users.noreply.github.com>
2024-08-30 12:20:48 -0600

    Add files via upload

---
+++ micadena.py
@@ -0,0 +1,8 @@
+# Mis Cadenas
+print("Manejo de cadenas")
+Mivecina="Lupita"
+Ocupacion="Ama de casa"
+ella=Mivecina+" "+Ocupacion
+print(ella)
+print("Otra Manera")
+print(f"Datos de la vecina {ella}")
---
+++ mivariables.py
@@ -0,0 +1,6 @@
+## Variables
+print("Mis Variables")
+edad=17
+nombre="Cristian Eduardo"
+print("Edad: ",edad)
+print("Nombre ",nombre)
